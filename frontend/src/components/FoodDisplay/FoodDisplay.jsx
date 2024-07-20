37ecee12edc195803c44b781c3f7c1daa3d30684
muhammadrazaq33 <muhammadrazaq5913483@gmail.com>
2024-07-20 04:21:04 -0700

    cards completed

---
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -0,0 +1,20 @@
+import React, { useContext } from 'react'
+import { StoreContext } from '../../Context/StoreContext'
+import FoodItem from '../FoodItem/FoodItem';
+// import { food_list } from '../../assets/assets'
+
+const FoodDisplay = () => {
+    const { food_list } = useContext(StoreContext);
+  return (
+    <div className="mt-[30px]">
+      <h1 className="Food-display-heading">Top dishes near you</h1>
+      <div className="Food-display-list">
+        {food_list.map((item) => {
+          return <FoodItem key={item._id} item={item} />;
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default FoodDisplay
---
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react'
+import { assets } from '../../assets/assets'
+
+const FoodItem = ({ item }) => {
+    const [itemCount, setItemCount] = useState(0);
+    const { _id, name, image, price, description } = item;
+  return (
+    <div className="food_item">
+      {/* item img */}
+      <div className="relative">
+        <img className="rounded-t-[15px] w-[100%]" src={image} alt="" />
+        {!itemCount ? (
+          <img
+            className="w-[35px] absolute bottom-[15px] right-[15px] rounded-[50%] cursor-pointer"
+            onClick={() => setItemCount((prev) => prev + 1)}
+            src={assets.add_icon_white}
+          />
+        ) : (
+          <div className="absolute bottom-[15px] right-[15px] flex items-center gap-[10px] bg-[white] p-[6px] rounded-[50px] " >
+            <img className='w-[30px]'
+              onClick={() => setItemCount((prev) => prev - 1)}
+              src={assets.remove_icon_red}
+              alt=""
+            />
+            <p>{itemCount}</p>
+            <img className='w-[30px]'
+              onClick={() => setItemCount((prev) => prev + 1)}
+              src={assets.add_icon_green}
+              alt=""
+            />
+          </div>
+        )}
+      </div>
+      {/* item info */}
+      <div className="p-[20px]">
+        <div className="flex justify-between items-center mb-[10px]">
+          <p className="text-[20px] font-medium">{name}</p>
+          <img className="w-[70px]" src={assets.rating_starts} alt="" />
+        </div>
+        <p className="text-[#676767] text-[12px]">{description}</p>
+        <p className="text-[tomato] text-[22px] my-[10px] font-medium">
+          ${price}
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default FoodItem
