8f32bb7befb112708c5578f3c0bbfa81ec1d4d58
muhammadrazaq33 <muhammadrazaq5913483@gmail.com>
2024-07-21 01:07:47 -0700

    footer css completed

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -3,41 +3,45 @@ import { assets } from '../../assets/assets'
 
 const Footer = () => {
   return (
-      <div>
-          <div>
-              {/* left side */}
-              <div>
-                  <img src={assets.logo} alt="" />
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, repudiandae est. Incidunt facilis neque rerum, voluptatem repudiandae quam nostrum? Repudiandae?</p>
-                  <div className='bg-black'>
-                      <img src={assets.facebook_icon} alt="" />
-                      <img src={assets.twitter_icon} alt="" />
-                      <img src={assets.linkedin_icon} alt="" />
-                  </div>
-              </div>
-              {/* center */}
-              <div>
-                  <h1>COMPANY</h1>
-                  <ul>
-                      <li>Home</li>
-                      <li>About Us</li>
-                      <li>Delivery</li>
-                      <li>Privacy Policy</li>
-                  </ul>
-              </div>
-              {/* right side */}
-              <div>
-                  <h2>GET IN TOUCH</h2>
-                  <ul>
-                      <li>+333-4538526</li>
-                      <li>muhammadrazaq5913483@gmail.com</li>
-                  </ul>
-              </div>
+    <div className="mt-[100px] text-[#d9d9d9] bg-[#323232] flex flex-col items-center py-[20px] px-[8vw] pt-[80px] gap-[20px]">
+      <div className="footer_content">
+        {/* left side */}
+        <div className="flex flex-col items-start gap-[20px]">
+          <img src={assets.logo} alt="" />
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem,
+            repudiandae est. Incidunt facilis neque rerum, voluptatem
+            repudiandae quam nostrum? Repudiandae?
+          </p>
+          <div className="flex gap-4">
+            <img className='w-[40px]' src={assets.facebook_icon} alt="" />
+            <img className='w-[40px]' src={assets.twitter_icon} alt="" />
+            <img className='w-[40px]' src={assets.linkedin_icon} alt="" />
           </div>
-          <hr />
-          <p>Copyright 2024 &copy; Tomato.com - All Right Reserved</p>
+        </div>
+        {/* center */}
+        <div className="flex flex-col items-start gap-[20px]">
+          <h1 className='text-white font-semibold'>COMPANY</h1>
+          <ul>
+            <li className="mb-[10px] cursor-pointer">Home</li>
+            <li className="mb-[10px] cursor-pointer">About Us</li>
+            <li className="mb-[10px] cursor-pointer">Delivery</li>
+            <li className="mb-[10px] cursor-pointer">Privacy Policy</li>
+          </ul>
+        </div>
+        {/* right side */}
+        <div className="flex flex-col items-start gap-[20px]">
+          <h2 className='text-white font-semibold'>GET IN TOUCH</h2>
+          <ul>
+            <li className="mb-[10px] cursor-pointer">+333-4538526</li>
+            <li className="mb-[10px] cursor-pointer">muhammadrazaq5913483@gmail.com</li>
+          </ul>
+        </div>
+      </div>
+      <hr className='w-[100%] h-[2px] my-[20px] mx-0 bg-[grey] border-none' />
+      <p>Copyright 2024 &copy; Tomato.com - All Right Reserved</p>
     </div>
-  )
+  );
 }
 
 export default Footer
