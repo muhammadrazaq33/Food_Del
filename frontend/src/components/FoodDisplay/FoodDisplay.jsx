bcc455e97c93591b1043147b7ae0594396cc6187
muhammadrazaq33 <muhammadrazaq5913483@gmail.com>
2024-07-20 23:53:48 -0700

    filter functionality completed

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -3,14 +3,16 @@ import { StoreContext } from '../../Context/StoreContext'
 import FoodItem from '../FoodItem/FoodItem';
 // import { food_list } from '../../assets/assets'
 
-const FoodDisplay = () => {
+const FoodDisplay = ({category}) => {
     const { food_list } = useContext(StoreContext);
   return (
     <div className="mt-[30px]">
       <h1 className="Food-display-heading">Top dishes near you</h1>
       <div className="Food-display-list">
         {food_list.map((item) => {
-          return <FoodItem key={item._id} item={item} />;
+          if (category === "All" || category === item.category){
+            return <FoodItem key={item._id} item={item} />;
+          }
         })}
       </div>
     </div>
