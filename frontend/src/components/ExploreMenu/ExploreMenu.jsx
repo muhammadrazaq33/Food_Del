228c73e63074a0ed4eabaa1b7a742f1e3099366f
muhammadrazaq33 <muhammadrazaq5913483@gmail.com>
2024-07-21 05:12:22 -0700

    footer also responsive

--- frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -5,7 +5,7 @@ const ExploreMenu = ({category,setCategory}) => {
   return (
     <div className='flex flex-col gap-[20px] mb-8'>
           <h1 className='text-[#262626] font-semibold text-[1.8rem]'>Explore our menu</h1>
-          <p className='text-[#808080] max-w-[60%]'>Choose from a diverse menu featuring a delectable array of dishes. Our mission is to satisfy your cravings and elevate your dining experience, one delicious meal at a time.</p>
+          <p className='text-[#808080] md:max-w-[60%] max-w-[100%] md:text-[17px] sm:text-[16px] text-[13px] '>Choose from a diverse menu featuring a delectable array of dishes. Our mission is to satisfy your cravings and elevate your dining experience, one delicious meal at a time.</p>
           <div className='flex justify-between items-center gap-[30px] text-center overflow-x-scroll scroll_hidden '>
               {menu_list.map((item, index) => {
                   return (
