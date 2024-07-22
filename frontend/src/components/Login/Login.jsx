a8291aba535b74f2d12b9105fedfe21a82058ef0
muhammadrazaq33 <muhammadrazaq5913483@gmail.com>
2024-07-22 00:57:58 -0700

    cart css also completed

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -4,41 +4,71 @@ import { assets } from '../../assets/assets';
 const Login = ({ setShowLogin }) => {
   const [currentState, setCurrentState] = useState("Sign up");
   return (
-    <div className="absolute z-[1] w-[100%] h-[100%] bg-[#00000090] grid">
-      <form className='login-container'>
-        <div className='flex justify-between items-center text-black'>
-          <h1 className='font-semibold text-[18px]'>{currentState}</h1>
-          <img className='w-[16px] cursor-pointer'
+    <div
+      className="absolute z-[1] w-[100%] h-[100%] bg-[#00000090] grid"
+    >
+      <form className="login-container">
+        <div className="flex justify-between items-center text-black">
+          <h1 className="font-semibold text-[18px]">{currentState}</h1>
+          <img
+            className="w-[16px] cursor-pointer"
             src={assets.cross_icon}
             onClick={() => setShowLogin(false)}
             alt=""
           />
         </div>
-        <div className='flex flex-col gap-[20px]'>
+        <div className="flex flex-col gap-[20px]">
           {currentState === "Login" ? (
             <></>
           ) : (
-            <input className='outline-none p-[10px] rounded-[4px] border-[1px] border-[#c9c9c9]' type="text" placeholder="Enter name" required />
+            <input
+              className="outline-none p-[10px] rounded-[4px] border-[1px] border-[#c9c9c9]"
+              type="text"
+              placeholder="Enter name"
+              required
+            />
           )}
-          <input className='outline-none p-[10px] rounded-[4px] border-[1px] border-[#c9c9c9]' type="email" placeholder="Enter email" required />
-          <input className='outline-none p-[10px] rounded-[4px] border-[1px] border-[#c9c9c9]' type="password" placeholder="Enter password" required />
+          <input
+            className="outline-none p-[10px] rounded-[4px] border-[1px] border-[#c9c9c9]"
+            type="email"
+            placeholder="Enter email"
+            required
+          />
+          <input
+            className="outline-none p-[10px] rounded-[4px] border-[1px] border-[#c9c9c9]"
+            type="password"
+            placeholder="Enter password"
+            required
+          />
         </div>
-        <button className='border-none p-[10px] rounded-[4px] cursor-pointer bg-[tomato] text-[15px] text-[white] '>
+        <button className="border-none p-[10px] rounded-[4px] cursor-pointer bg-[tomato] text-[15px] text-[white] ">
           {currentState === "Sign up" ? "Create Account" : "Login"}
         </button>
-        <div className='flex items-start gap-[8px] -mt-[15px]'>
-          <input type="checkbox" className='mt-[5.8px]' required />
+        <div className="flex items-start gap-[8px] -mt-[15px]">
+          <input type="checkbox" className="mt-[5.8px]" required />
           <p>By continuing, i agree to the terms of use and privacy policy.</p>
         </div>
         {currentState === "Login" ? (
           <p>
             Create a new account?{" "}
-            <span onClick={() => setCurrentState("Sign up")}  className='cursor-pointer text-[tomato] font-semibold' > Click here</span>
+            <span
+              onClick={() => setCurrentState("Sign up")}
+              className="cursor-pointer text-[tomato] font-semibold"
+            >
+              {" "}
+              Click here
+            </span>
           </p>
         ) : (
           <p>
             Already have an account?{" "}
-            <span onClick={() => setCurrentState("Login")} className='cursor-pointer text-[tomato] font-semibold'> Login here</span>
+            <span
+              onClick={() => setCurrentState("Login")}
+              className="cursor-pointer text-[tomato] font-semibold"
+            >
+              {" "}
+              Login here
+            </span>
           </p>
         )}
       </form>
--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -4,9 +4,9 @@ import { StoreContext } from '../../Context/StoreContext'
 const Cart = () => {
   const {cartItem,food_list, removeFromCart} = useContext(StoreContext)
   return (
-    <div>
+    <div className="mt-[100px]">
       <div>
-        <div>
+        <div className="cart-item-title">
           <p>Items</p>
           <p>Title</p>
           <p>Price</p>
@@ -20,19 +20,67 @@ const Cart = () => {
           if (cartItem[item._id] > 0) {
             return (
               <div>
-                <img src={item.image} alt="" />
-                <p>{item.name}</p>
-                <p>{item.price}</p>
-                <p>{cartItem[item._id]}</p>
-                <p>{item.price * cartItem[item._id]}</p>
-                <p>x</p>
+                <div className="cart-item-title cart-item">
+                  <img className="w-[50px]" src={item.image} alt="" />
+                  <p>{item.name}</p>
+                  <p>${item.price}</p>
+                  <p>{cartItem[item._id]}</p>
+                  <p>${item.price * cartItem[item._id]}</p>
+                  <p
+                    className="cursor-pointer"
+                    onClick={() => removeFromCart(item._id)}
+                  >
+                    x
+                  </p>
+                </div>
+                <hr className="h-[1px]  border-none bg-[#e2e2e2]" />
               </div>
             );
           }
         })}
       </div>
+      {/* total price - side bar */}
+      <div className="mt-[80px] flex md:flex-row flex-col-reverse  justify-between sm:gap-[12vw] gap-[20px]">
+        <div className="flex-1 flex flex-col gap-[20px]">
+          <h1>Cart Totals</h1>
+          <div>
+            <div className="flex justify-between text-[#555]">
+              <p>Subtotal</p>
+              <p>{0}</p>
+            </div>
+            <hr className="my-[10px]" />
+            <div className="flex justify-between text-[#555]">
+              <p>Delivery Fee</p>
+              <p>{2}</p>
+            </div>
+            <hr className="my-[10px]" />
+            <div className="flex justify-between text-[#555]">
+              <b>Total</b>
+              <b>{0}</b>
+            </div>
+          </div>
+          <button className="cart-btn">PROCEED TO CHECKOUT</button>
+        </div>
+        {/* promo code */}
+        <div className="flex-1   ">
+          <div>
+            <p className="text-[#555]">
+              If you have a promo code, Enter it here
+            </p>
+            <div className="mt-[10px] flex justify-between items-center bg-[#eaeaea] rounded-[4px]">
+              <input
+                className="border-none outline-none pl-[10px] bg-transparent"
+                type="text"
+                placeholder="promo code"
+              />
+              <button className="promo-btn">Submit</button>
+            </div>
+          </div>
+        </div>
+        {/* end of promo code */}
+      </div>
     </div>
-  )
+  );
 }
 
 export default Cart
