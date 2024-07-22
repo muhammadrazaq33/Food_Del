15776fd0aae8ce09667798b94dc58a268b5479e2
muhammadrazaq33 <muhammadrazaq5913483@gmail.com>
2024-07-21 22:53:58 -0700

    html of cart page completed

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -6,31 +6,37 @@ const Navbar = ({setShowLogin}) => {
     const [menu, setMenu] = useState("home");
   return (
     <div className="flex justify-between items-center py-[20px]">
-      <img
-        src={assets.logo}
-        alt=""
-        className=" lg:w-[150px] md:w-[120px] w-[100px] lg:h-[33px] md:h-[28px] h-[24px]"
-      />
+      <Link to={"/"}>
+        <img
+          src={assets.logo}
+          alt=""
+          className=" lg:w-[150px] md:w-[120px] w-[100px] lg:h-[33px] md:h-[28px] h-[24px]"
+        />
+      </Link>
       <ul className="md:flex hidden lg:gap-x-[20px] md:gap-x-[13px]  text-[#49557e] lg:text-[18px] md:text-[15px]">
-        <Link to={"/"}
+        <Link
+          to={"/"}
           onClick={() => setMenu("home")}
           className={`cursor-pointer ${menu === "home" ? "active" : ""}`}
         >
           home
         </Link>
-        <a href='#explore-menu'
+        <a
+          href="#explore-menu"
           onClick={() => setMenu("menu")}
           className={`${menu === "menu" ? "active" : ""} cursor-pointer`}
         >
           menu
         </a>
-        <a href='#app-download'
+        <a
+          href="#app-download"
           onClick={() => setMenu("mobile-app")}
           className={` cursor-pointer ${menu === "mobile-app" ? "active" : ""}`}
         >
           mobile-app
         </a>
-        <a href='#contact-us'
+        <a
+          href="#contact-us"
           onClick={() => setMenu("contact-us")}
           className={` cursor-pointer ${menu === "contact-us" ? "active" : ""}`}
         >
@@ -41,14 +47,20 @@ const Navbar = ({setShowLogin}) => {
       <div className="flex items-center lg:gap-[40px] md:gap-[24px] sm:gap-[20px] gap-[16px]">
         <img src={assets.search_icon} className="lg:w-[26px] w-[20px]" alt="" />
         <div className="relative">
-          <img
-            src={assets.basket_icon}
-            className="lg:w-[26px] w-[22px]"
-            alt=""
-          />
+          <Link to={"/cart"}>
+            {" "}
+            <img
+              src={assets.basket_icon}
+              className="lg:w-[26px] w-[22px]"
+              alt=""
+            />
+          </Link>
           <div className="absolute min-w-[10px] min-h-[10px] bg-[tomato] rounded-[5px] top-[-8px] right-[-8px]"></div>
         </div>
-        <button onClick={() => setShowLogin(true)} className="bg-transparent lg:text-[16px] text-[15px]   lg:px-[30px] md:px-[25px] px-[18px] lg:py-[10px] md:py-[7px] py-[3px] border border-[tomato] border-solid rounded-[50px] text-[#49557e] hover:bg-[#fff4f2] duration-1000">
+        <button
+          onClick={() => setShowLogin(true)}
+          className="bg-transparent lg:text-[16px] text-[15px]   lg:px-[30px] md:px-[25px] px-[18px] lg:py-[10px] md:py-[7px] py-[3px] border border-[tomato] border-solid rounded-[50px] text-[#49557e] hover:bg-[#fff4f2] duration-1000"
+        >
           sign in
         </button>
       </div>
--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { StoreContext } from '../../Context/StoreContext'
 
 const Cart = () => {
+  const {cartItem,food_list, removeFromCart} = useContext(StoreContext)
   return (
     <div>
-      
+      <div>
+        <div>
+          <p>Items</p>
+          <p>Title</p>
+          <p>Price</p>
+          <p>Quantity</p>
+          <p>Total</p>
+          <p>Remove</p>
+        </div>
+        <br />
+        <hr />
+        {food_list.map((item) => {
+          if (cartItem[item._id] > 0) {
+            return (
+              <div>
+                <img src={item.image} alt="" />
+                <p>{item.name}</p>
+                <p>{item.price}</p>
+                <p>{cartItem[item._id]}</p>
+                <p>{item.price * cartItem[item._id]}</p>
+                <p>x</p>
+              </div>
+            );
+          }
+        })}
+      </div>
     </div>
   )
 }
