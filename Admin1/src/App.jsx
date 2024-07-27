96e49ef4aa432bc5ef028f3451da9d5c17277941
muhammadrazaq33 <muhammadrazaq5913483@gmail.com>
2024-07-27 08:35:18 -0700

    route set up completed

--- Admin1/src/App.jsx
+++ Admin1/src/App.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import SideBar from './components/SideBar/SideBar'
+import { Route, Routes } from 'react-router-dom'
+import Add from "../src/pages/Add/Add"
+import List from "../src/pages/List/List"
+import Orders from "../src/pages/Orders/Orders"
 
 const App = () => {
   return (
@@ -8,7 +12,12 @@ const App = () => {
       <Navbar />
       <hr />
       <div className='app-content'>
-        <SideBar/>
+        <SideBar />
+        <Routes>
+          <Route path='/add' element={<Add /> } />
+          <Route path='/list' element={<List /> } />
+          <Route path='/orders' element={<Orders /> } />
+        </Routes>
       </div>
     </div>
   )
