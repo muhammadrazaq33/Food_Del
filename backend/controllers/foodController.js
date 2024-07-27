88ba04f6561dc519a29d49b591c166384f664f19
muhammadrazaq33 <muhammadrazaq5913483@gmail.com>
2024-07-27 03:44:51 -0700

    add food api successfully work

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -7,12 +7,12 @@ const addFood = async (req, res) => {
 
     let image_filename = `${req.file.filename}`;
 
-    const food = ({
+    const food = new foodModel({
         name: req.body.name,
         description: req.body.description,
         price: req.body.price,
+        category: req.body.category,
         image: image_filename,
-        category: req.body.category
     })
 
     try {
