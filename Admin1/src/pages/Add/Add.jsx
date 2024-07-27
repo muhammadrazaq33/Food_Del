ca0865b4067c11fae48d823579e3dab55a760d52
muhammadrazaq33 <muhammadrazaq5913483@gmail.com>
2024-07-27 09:08:16 -0700

    add page UI completed

--- Admin1/src/pages/Add/Add.jsx
+++ Admin1/src/pages/Add/Add.jsx
@@ -1,12 +1,49 @@
 import React from 'react'
 import "./Add.css"
+import { assets } from '../../assets/assets'
 
 const Add = () => {
   return (
-    <div>
-      
+    <div className="add">
+      <form className="flex-col1">
+        <div className="add-image-upload flex-col1">
+          <p>Upload Image</p>
+          <label htmlFor="image">
+            <img src={assets.upload_area} alt="" />
+          </label>
+          <input type="file" id="image" hidden required />
+        </div>
+        <div className="add-product-name flex-col1">
+          <p>Product name</p>
+          <input type="text" name="name" placeholder="Type here" />
+        </div>
+        <div className="add-product-description flex-col1">
+          <p>Product description</p>
+          <textarea name="description" rows="6" placeholder='Write content here' required></textarea>
+        </div>
+        <div className='add-category-price'>
+          <div className="add-category flex-col1">
+            <p>Product Category</p>
+            <select name="category">
+              <option value="Salad">Salad</option>
+              <option value="Rolls">Rolls</option>
+              <option value="Deserts">Deserts</option>
+              <option value="Sandwich">Sandwich</option>
+              <option value="Cake">Cake</option>
+              <option value="Pure veg">Pure veg</option>
+              <option value="Pasta">Pasta</option>
+              <option value="Noodles">Noodles</option>
+            </select>
+          </div>
+          <div className="add-price flex-col1">
+            <p>Product Price</p>
+            <input type="Number" name='price' placeholder='$20'/>
+          </div>
+        </div>
+        <button type='submit' className='add-btn'>ADD</button>
+      </form>
     </div>
-  )
+  );
 }
 
 export default Add
