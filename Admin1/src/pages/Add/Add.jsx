f98eb89cdffcb87e551ce1afa225d9d960146999
muhammadrazaq33 <muhammadrazaq5913483@gmail.com>
2024-07-28 00:11:23 -0700

    add food item successfully integrated with frontend

--- Admin1/src/pages/Add/Add.jsx
+++ Admin1/src/pages/Add/Add.jsx
@@ -1,30 +1,95 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./Add.css"
 import { assets } from '../../assets/assets'
+import axios from "axios";
 
 const Add = () => {
+
+  const url = "http://localhost:4000"
+
+  const [image, setImage] = useState(false);
+  const [data, setData] = useState({
+    name: "",
+    description: "",
+    price: "",
+    category: "Salad"
+  });
+
+  const onChangeHandler = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setData(data=>({...data,[name]:value}))
+  }
+
+  const onSubmitData = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("name",data.name)
+    formData.append("description",data.description)
+    formData.append("price", Number(data.price));
+    formData.append("category",data.category)
+    formData.append("image", image)
+    const response = await axios.post(`${url}/api/food/add`, formData);
+    if (response.data.success) {
+      setData({
+        name: "",
+        description: "",
+        price: "",
+        category: "Salad",
+      });
+      setImage(false);
+    } else {
+      
+    }
+  }
+
+  useEffect(() => {
+    console.log(data)
+  },[data])
   return (
     <div className="add">
-      <form className="flex-col1">
+      <form className="flex-col1" onSubmit={onSubmitData}>
         <div className="add-image-upload flex-col1">
           <p>Upload Image</p>
           <label htmlFor="image">
-            <img src={assets.upload_area} alt="" />
+            <img
+              src={image ? URL.createObjectURL(image) : assets.upload_area}
+              alt=""
+            />
           </label>
-          <input type="file" id="image" hidden required />
+          <input
+            type="file"
+            onChange={(e) => setImage(e.target.files[0])}
+            id="image"
+            hidden
+            required
+          />
         </div>
         <div className="add-product-name flex-col1">
           <p>Product name</p>
-          <input type="text" name="name" placeholder="Type here" />
+          <input
+            type="text"
+            onChange={onChangeHandler}
+            value={data.name}
+            name="name"
+            placeholder="Type here"
+          />
         </div>
         <div className="add-product-description flex-col1">
           <p>Product description</p>
-          <textarea name="description" rows="6" placeholder='Write content here' required></textarea>
+          <textarea
+            name="description"
+            onChange={onChangeHandler}
+            value={data.description}
+            rows="6"
+            placeholder="Write content here"
+            required
+          ></textarea>
         </div>
-        <div className='add-category-price'>
+        <div className="add-category-price">
           <div className="add-category flex-col1">
             <p>Product Category</p>
-            <select name="category">
+            <select onChange={onChangeHandler} name="category">
               <option value="Salad">Salad</option>
               <option value="Rolls">Rolls</option>
               <option value="Deserts">Deserts</option>
@@ -37,10 +102,18 @@ const Add = () => {
           </div>
           <div className="add-price flex-col1">
             <p>Product Price</p>
-            <input type="Number" name='price' placeholder='$20'/>
+            <input
+              type="Number"
+              onChange={onChangeHandler}
+              value={data.price}
+              name="price"
+              placeholder="$20"
+            />
           </div>
         </div>
-        <button type='submit' className='add-btn'>ADD</button>
+        <button type="submit" className="add-btn">
+          ADD
+        </button>
       </form>
     </div>
   );
