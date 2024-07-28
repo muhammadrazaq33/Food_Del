726a882d1aec51d3f928d2c0c50fd7ec369e95cd
muhammadrazaq33 <muhammadrazaq5913483@gmail.com>
2024-07-28 04:55:16 -0700

    register and login api structure completed

---
+++ backend/controllers/userController.js
@@ -0,0 +1,17 @@
+import userModel from "../models/UserModel";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import validator from "validator";
+
+
+const userLogin = async () => {
+ 
+}
+
+// user Register 
+const userRegister = async () => {
+
+} 
+
+
+export { userLogin, userRegister };
