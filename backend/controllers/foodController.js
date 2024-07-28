2d8a466d3d416802b79f7a7430ea9c62a9e70176
muhammadrazaq33 <muhammadrazaq5913483@gmail.com>
2024-07-28 01:00:32 -0700

    list all item api successfully integrated

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -28,7 +28,7 @@ const addFood = async (req, res) => {
 const listFood = async (req,res) => {
     try {
         const foods = await foodModel.find();
-        res.json({success:true, data:foods})
+        res.json({ success: true, data: foods });
     } catch (error) {
         console.log(error)
         res.json({success:false,message: "Error"})
