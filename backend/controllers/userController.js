1771649a9c23721d97bbf0375f0ddc407aa79b3c
muhammadrazaq33 <muhammadrazaq5913483@gmail.com>
2024-08-01 01:16:35 -0700

    register api successfully work

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,15 +1,57 @@
-import userModel from "../models/UserModel";
+import userModel from "../models/UserModel.js";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 import validator from "validator";
 
 
-const userLogin = async () => {
+const userLogin = async (req,res) => {
  
 }
 
+// create token
+const createToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET);
+}
+
+
 // user Register 
-const userRegister = async () => {
+const userRegister = async (req,res) => {
+    const { name, email, password } = req.body;
+    try {
+        // checking is user already exist
+        let exist = await userModel.findOne({ email });
+    if (exist) {
+        return res.json({success:false,message:"User already exist."})
+    }
+
+    // validating email & password
+    if (!validator.isEmail(email)) {
+   return res.json({success:false,message:"Please enter a valid email."})
+    }
+
+    if (password.length < 8) {
+           return res.json({success:false,message:"Please enter a strong password."})
+    }
+
+    //hashing user password
+    const salt = await bcrypt.genSalt(10);
+       const hashedPassword = await bcrypt.hash(password, salt);
+       
+       const newUser = new userModel({
+           name: name,
+           email: email,
+           password:hashedPassword
+       })
+
+        const user = await newUser.save();
+        console.log(user)
+       const token = createToken(user._id);
+       res.json({ success: true, token });
+
+   } catch (error) {
+       console.log(error);
+       res.json({ success: false, message: "Error" })
+   }
 
 } 
 
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { userLogin, userRegister } from "../controllers/userController";
+import { userLogin, userRegister } from "../controllers/userController.js";
 
 
 const userRouter = express.Router();
