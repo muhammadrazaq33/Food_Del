bc551587bc88e95e59b7c7c83c95823c263b2b5b
muhammadrazaq33 <muhammadrazaq5913483@gmail.com>
2024-08-04 00:00:21 -0700

    add to cart api successfully work

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -3,7 +3,21 @@ import userModel from "../models/UserModel.js";
 
 // add to cart
 const addToCart = async (req, res) => {
-    
+    try {
+        const userData = await userModel.findOne({ _id: req.body.userId });
+        const cartData = await userData.cartData;
+        if (!cartData[req.body.itemId]) {
+            cartData[req.body.itemId] = 1;
+        } else {
+            cartData[req.body.itemId] += 1;
+        }
+        await userModel.findByIdAndUpdate(req.body.userId, { cartData })
+        res.json({success:true, message:"Added to cart."})
+    } catch (error) {
+        console.log(error);
+        res.json({success:false, message:"Error"})
+
+    }
 }
 
 // remove from cart
