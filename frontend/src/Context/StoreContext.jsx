17b441ab3b9d9280abe394e929d99c73cf3e9de7
muhammadrazaq33 <muhammadrazaq5913483@gmail.com>
2024-08-04 00:51:15 -0700

    get cart item api successfully integrated with react

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -48,13 +48,19 @@ const StoreContextProvider = (props) => {
   const fetchFoodList = async () => {
     const response = await axios.get(url + "/api/food/list");
     setFoodList(response.data.data);
-}
+  }
+  
+  const loadCartData = async (token) => {
+    const response = await axios.post(url + "/api/cart/get", {}, { headers: { token } });
+    setCartItem(response.data.cartData);
+  }
 
   useEffect(() => {
     async function loadData() {
       await fetchFoodList();
     if (localStorage.getItem("token")) {
       setToken(localStorage.getItem("token"));
+      await loadCartData(localStorage.getItem("token"));
     }
     }
     loadData();
